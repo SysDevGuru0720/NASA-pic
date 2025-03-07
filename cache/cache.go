c07477d19120efb7eddbee63dea6250ec0e812ef
SysDevGuru0720 <leahucosmin0720@gmail.com>
2025-03-07 06:48:59 -0500

    Make a func to create new redis object.

---
+++ cache/cache.go
@@ -0,0 +1,30 @@
+package cache
+
+import (
+	"log"
+
+	"github.com/gomodule/redigo/redis"
+	"google.golang.org/genproto/googleapis/cloud/redis/v1"
+)
+
+var (
+	pool *redis.Pool
+)
+
+func init() {
+	pool = newPool()
+}
+
+func newPool() *redis.Pool {
+	return &redis.Pool{
+		MaxIdle:   80,
+		MaxActive: 12000,
+		Dial: func() (redis.Conn, error) {
+			c, err := redis.Dial("tcp", ":6379")
+			if err != nil {
+				log.Printf("nasapic: failed to dial redis: %v\n", err)
+			}
+			return c, err
+		},
+	}
+}
