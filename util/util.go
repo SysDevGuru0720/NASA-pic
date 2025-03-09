45c2a6ce46dd269ad50d0ec535d78c332d39e56c
SysDevGuru0720 <leahucosmin0720@gmail.com>
2025-03-09 13:46:37 -0400

    Make a func to parse template.

--- util/util.go
+++ util/util.go
@@ -1,5 +1,32 @@
 package util
 
+import (
+	"bytes"
+	"html/template"
+	"os"
+
+	"github.com/SysDevGuru0720/NASA-pic/config"
+)
+
 func ParseTemplate(filePath string, data interface{}) error {
+	t, err := template.ParseFiles(filePath)
+	if err != nil {
+		return err
+	}
+	buf := new(bytes.Buffer)
+	if err := t.Execute(buf, data); err != nil {
+		return err
+	}
+
+	f, err := os.Create(config.Global.Config.IndexPath)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	if _, err = f.Write([]byte(buf.String())); err != nil {
+		return err
+	}
+
 	return nil
 }
