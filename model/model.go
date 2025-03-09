e81561e96f16735ecb4e07df639e80d3b3aa2865
SysDevGuru0720 <leahucosmin0720@gmail.com>
2025-03-08 19:38:43 -0500

    Make a func to fetch picture from Nasa.

--- model/model.go
+++ model/model.go
@@ -2,9 +2,13 @@ package model
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"log"
+	"net/http"
+	"os"
 
 	"github.com/SysDevGuru0720/NASA-pic/cache"
+	"github.com/SysDevGuru0720/NASA-pic/config"
 	"github.com/gomodule/redigo/redis"
 )
 
@@ -41,5 +45,39 @@ func (p *Picture) GetPic() error {
 }
 
 func (p *Picture) fetchPicture(date string, conn redis.Conn) error {
+	defer conn.Close()
+	client := &http.Client{}
 
+	req, err := http.NewRequest("GET", config.Global.Config.NasaURL, nil)
+	if err != nil {
+		return err
+	}
+
+	q := req.URL.Query()
+	q.Add("date", date)
+	q.Add("api_key", os.Getenv("NASA_API_KEY"))
+	req.URL.RawQuery = q.Encode()
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(respBody, &p)
+	if err != nil {
+		return err
+	}
+
+	err = cache.Set(conn, date, string(respBody))
+	if err != nil {
+		log.Printf("nasapic: failed to set cache: %v\n", err)
+	}
+
+	return nil
 }
